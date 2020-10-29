000b5e9dfeb3901fcc7cdb50f3d9574ab5311c84
Harsh <thehjain@gmail.com>
2020-10-29 17:49:18 +0530

    Longest Common Prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,39 @@
+//Here we have given an array of stings
+//we have to find the longest common prefix
+//among all the string present in array
+
+//Brute Force solution is to use two loops and
+//check character one by one
+
+//Optimal solution is to use binary search and
+//check the longest common prefix.
+
+
+public String longestCommonPrefix(String[] strs) {
+	if (strs == null || strs.length == 0)
+		return "";
+	int minLen = Integer.MAX_VALUE;
+	for (String str : strs)
+		minLen = Math.min(minLen, str.length());
+	int low = 1;
+	int high = minLen;
+	while (low <= high) {
+		int middle = (low + high) / 2;
+		if (isCommonPrefix(strs, middle))
+			low = middle + 1;
+		else
+			high = middle - 1;
+	}
+	return strs[0].substring(0, (low + high) / 2);
+}
+
+private boolean isCommonPrefix(String[] strs, int len) {
+	String str1 = strs[0].substring(0, len);
+	for (int i = 1; i < strs.length; i++)
+		if (!strs[i].startsWith(str1))
+			return false;
+	return true;
+}
+
+
+//problem is available on leetcode
